e29af0e1c0203762dd30c092b43b07eb0aa8cd67
Ian Shehadeh <IanShehadeh2020@gmail.com>
2022-08-21 09:34:30 -0400

    sort User fields alphabetically
    It's helful to tkeep the field order the same as in the documentation.
    Much easier to compare differences when updating versions

--- src/resource/user.rs
+++ src/resource/user.rs
@@ -24,78 +24,6 @@ pub struct Collaborator {
 
 /// A Todoist user
 pub struct User {
-    /// The user's ID
-    pub id: ID,
-
-    /// The user's API token
-    pub token: String,
-
-    /// The user's email
-    pub email: String,
-
-    /// the user's full name, formatted as "Firstname Lastname"
-    pub full_name: String,
-
-    /// the ID of the user's inbox project
-    pub inbox_project: ID,
-
-    /// the user's timezone info
-    pub tz_info: TimeZoneInfo,
-
-    /// the user's default view on todoist
-    pub start_page: String,
-
-    /// the first day of the week, between 1 and 7
-    pub start_day: isize,
-
-    /// the day of next week that tasks will be postponed to
-    pub next_week: isize,
-
-    /// the format for time, 24h or 12h
-    pub time_format: isize,
-
-    /// the date format, DD-MM-YY or MM-DD-YY
-    pub date_format: isize,
-
-    /// the order to sort items, newest first (1), or oldest first (0)
-    pub sort_order: isize,
-
-    /// the default reminder method
-    pub default_reminder: NotificationService,
-
-    /// the user's phone number
-    pub mobile_number: Option<String>,
-
-    /// the user's mobile host
-    pub mobile_host: Option<String>,
-
-    /// the total number of completed tasks
-    pub completed_count: isize,
-
-    /// tasks completed today
-    pub completed_today: isize,
-
-    /// the user's karma score
-    pub karma: f64,
-
-    /// the user's karma trend, e.g. up
-    pub karma_trend: String,
-
-    /// false if this user is a peasant
-    pub is_premium: bool,
-
-    /// when this user's premium ends
-    pub premium_until: Option<String>,
-
-    /// if this user is a business account admin
-    pub is_biz_admin: bool,
-
-    /// the ID of the user's business account
-    pub business_account_id: Option<ID>,
-
-    /// the ID of this user's avatar
-    pub image_id: Option<String>,
-
     /// the user's small avatar
     pub avatar_small: Option<String>,
 
@@ -105,13 +33,85 @@ pub struct User {
     /// the user's big avatar
     pub avatar_big: Option<String>,
 
-    /// the user's UI theme (a number between 0 and 10)
-    pub theme: isize,
+    /// the ID of the user's business account
+    pub business_account_id: Option<ID>,
+
+    /// the total number of completed tasks
+    pub completed_count: isize,
+
+    /// tasks completed today
+    pub completed_today: isize,
+
+    /// the date format, DD-MM-YY or MM-DD-YY
+    pub date_format: isize,
+
+    /// the default reminder method
+    pub default_reminder: NotificationService,
+
+    /// The user's email
+    pub email: String,
 
     #[serde(skip)]
     /// used internally by todoist
     pub features: Option<()>,
 
+    /// the user's full name, formatted as "Firstname Lastname"
+    pub full_name: String,
+
+    /// The user's ID
+    pub id: ID,
+
+    /// the ID of this user's avatar
+    pub image_id: Option<String>,
+
+    /// the ID of the user's inbox project
+    pub inbox_project: ID,
+
+    /// if this user is a business account admin
+    pub is_biz_admin: bool,
+
+    /// false if this user is a peasant
+    pub is_premium: bool,
+
     /// When the user joined
     pub join_date: Date,
+
+    /// the user's karma score
+    pub karma: f64,
+
+    /// the user's karma trend, e.g. up
+    pub karma_trend: String,
+
+    /// the user's phone number
+    pub mobile_number: Option<String>,
+
+    /// the user's mobile host
+    pub mobile_host: Option<String>,
+
+    /// the day of next week that tasks will be postponed to
+    pub next_week: isize,
+
+    /// when this user's premium ends
+    pub premium_until: Option<String>,
+
+    /// the order to sort items, newest first (1), or oldest first (0)
+    pub sort_order: isize,
+
+    /// the user's default view on todoist
+    pub start_page: String,
+
+    /// the first day of the week, between 1 and 7
+    pub start_day: isize,
+
+    /// The user's API token
+    pub token: String,
+
+    /// the format for time, 24h or 12h
+    pub time_format: isize,
+
+    /// the user's UI theme (a number between 0 and 10)
+    pub theme: isize,
+
+    /// the user's timezone info
+    pub tz_info: TimeZoneInfo,
 }
