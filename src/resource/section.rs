c76f6a355a23c4762659031de26b82b1ead07951
Ian Shehadeh <IanShehadeh2020@gmail.com>
2022-08-24 09:20:01 -0400

    add Section properties

--- src/resource/section.rs
+++ src/resource/section.rs
@@ -1,6 +1,32 @@
+use chrono::{DateTime, Utc};
+
 #[derive(Deserialize, Serialize, Debug, Clone)]
 #[cfg_attr(test, serde(deny_unknown_fields))]
-pub struct Section {}
+pub struct Section {
+    pub id: String,
+    pub name: String,
+    pub project_id: String,
+
+    /// The place of this section among its siblings
+    pub section_order: i32,
+
+    /// Undocumented, I assume its the ID of the user who created the section.
+    pub user_id: String,
+
+    /// True if the sections child tasks are hidden
+    pub collapsed: bool,
+
+    /// ID used to identify this section between users in shared proejcts, `id` is per-user.
+    pub sync_id: Option<String>,
+
+    pub is_deleted: bool,
+
+    pub is_archived: bool,
+
+    pub archived_at: Option<DateTime<Utc>>,
+
+    pub added_at: DateTime<Utc>,
+}
 
 #[cfg(test)]
 mod test {
